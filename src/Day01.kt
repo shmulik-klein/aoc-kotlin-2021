87b5fbec48998afeea8495553dcf3ca0ac42d3c4
shmulik-klein <shmulik.klein@gmail.com>
2021-12-03 14:46:16 +0200

    day 1 initial solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,41 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        var lastDepth = input[0]
+        for (depth in input) {
+            if (lastDepth < depth) {
+                count++
+            }
+            lastDepth = depth
+        }
+        return count
     }
+
 
     fun part2(input: List<String>): Int {
-        return input.size
+        fun getWindowSum(input: List<String>, windowsIndex: Int): Int {
+            return input[windowsIndex].toInt() + input[windowsIndex + 1].toInt() + input[windowsIndex + 2].toInt()
+        }
+
+        var lastWindowSum = getWindowSum(input, 0)
+        var count = 0
+        for (i in 1..input.size - 3) {
+            val currentWindowSum = getWindowSum(input, i)
+            if (lastWindowSum < currentWindowSum) {
+                count++
+            }
+            lastWindowSum = currentWindowSum
+        }
+        return count
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    // part 1
+    val input = readInput("day1_1-input")
+    val part1Result = part1(input)
+    println("The number of increments is $part1Result")
+
+    // part 2
+    val part2Result = part2(input)
+    println("The number of increments is $part2Result")
 }
