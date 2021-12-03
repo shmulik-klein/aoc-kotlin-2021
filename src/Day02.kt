3273f7235cd52e8cbbdbb8eb7eab8e733cf98bf0
shmulik-klein <shmulik.klein@gmail.com>
2021-12-03 15:56:14 +0200

    day 2, half 2 initial solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -15,20 +15,24 @@ fun main() {
 
 
     fun part2(input: List<String>): Int {
-        fun getWindowSum(input: List<String>, windowsIndex: Int): Int {
-            return input[windowsIndex].toInt() + input[windowsIndex + 1].toInt() + input[windowsIndex + 2].toInt()
-        }
-
-        var lastWindowSum = getWindowSum(input, 0)
-        var count = 0
-        for (i in 1..input.size - 3) {
-            val currentWindowSum = getWindowSum(input, i)
-            if (lastWindowSum < currentWindowSum) {
-                count++
+        var (xPos, yPos, aim) = arrayOf(0, 0, 0)
+        for (line in input) {
+            val amount = line.split(" ").last().toInt()
+            val command = line.split(" ").first()
+            when (command.lowercase()) {
+                "forward" -> {
+                    xPos += amount
+                    yPos += (aim * amount)
+                }
+                "up" -> {
+                    aim -= amount
+                }
+                "down" -> {
+                    aim += amount
+                }
             }
-            lastWindowSum = currentWindowSum
         }
-        return count
+        return xPos * yPos
     }
 
 
@@ -37,7 +41,7 @@ fun main() {
     val part1Result = part1(input)
     println("The number of increments is $part1Result")
 
-//    // part 2
-//    val part2Result = part2(input)
-//    println("The number of increments is $part2Result")
+    // part 2
+    val part2Result = part2(input)
+    println("The number of increments is $part2Result")
 }
